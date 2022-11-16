bf3f2d6dc9c8aeef4faf33a6c1abe39ab2f857c3
omartheironman <58672497+omartheironman@users.noreply.github.com>
2022-11-16 09:56:37 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -9,7 +9,7 @@ def get_auth(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
+            detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Basic"},
         )
     return credentials.username
